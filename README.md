9816d3c3c49cf7b6fe23f665cc9857d5b42df5e6
Kin Thai <kinthai@gmail.com>
2016-03-13 17:20:53 -0700

    bootstrap complete

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # jen-and-kin
 Jen and Kin's Website
-
-# To generate bundle:
-# browserify -t [ babelify --presets [ es2015 react ] ] src/main.js -o public/js/bundle.js
-# or: gulp build
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var fs = require("fs");
 var browserify = require("browserify");
 
+// browserify -t [ babelify --presets [ es2015 react ] ] src/main.js -o public/js/bundle.js
 gulp.task('build', function() {
   browserify('src/main.js')
     .transform('babelify', {presets: ['es2015', 'react']})
