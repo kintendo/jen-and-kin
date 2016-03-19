0f8d7f011f186990785bd7f5dda3b89e642263d3
Kin Thai <kinthai@gmail.com>
2016-03-19 00:30:00 -0700

    add gulp sass and git ignore

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var fs = require("fs");
 var browserify = require("browserify");
+var sass = require('gulp-sass');
 
 // browserify -t [ babelify --presets [ es2015 react ] ] src/main.js -o public/js/bundle.js
 gulp.task('build', function() {
@@ -12,6 +13,13 @@ gulp.task('build', function() {
     .pipe(fs.createWriteStream('public/js/bundle.js'));
 });
 
+gulp.task('sass', function () {
+  gulp.src('sass/*')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/css'));
+});
+
 gulp.task('watch', function () {
   gulp.watch('src/*', ['build']);
+  gulp.watch('sass/*', ['sass']);
 });
