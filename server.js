f6026980ccf5426fd794a4eb13f870ced7b60ddc
Kin Thai <kinthai@gmail.com>
2016-05-06 09:17:17 -0700

    turn site on

--- server.js
+++ server.js
@@ -5,15 +5,9 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3001;
 
-//app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.send('Coming soon...');
-});
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/supersecret', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
